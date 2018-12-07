49b8eb6d7d70d8d791d3ba38d24283ebe359ef51
Sebastian Ruiz <supasebie@gmail.com>
2018-12-07 07:58:53 -0800

    Initial commit working skeleton

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+    //Config Params
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
